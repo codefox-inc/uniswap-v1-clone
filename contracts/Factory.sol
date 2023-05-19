5a4568bedbf6dda7ea8f43e622310842c4e19c68
thurendous <thurendous2@gmail.com>
2023-05-19 11:46:37 +0900

    ✅  add commnets and graph

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,22 +1,23 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
-import './Exchange.sol';
+import './Exchange.sol'; // Exchange.solをインポートして、継承はしない
 
 contract Factory {
-    mapping(address => address) public tokenToExchange;
+    mapping(address => address) public tokenToExchange; // toekn -> Exchangeのマッピング、factoryにてExchangeのアドレスを記録している
 
     function createExchange(address _tokenAddress) public returns (address) {
-        require(_tokenAddress != address(0), 'invalid token address');
-        require(tokenToExchange[_tokenAddress] == address(0), 'exchange already exists');
+        // Exchangeをデプロイする関数
+        require(_tokenAddress != address(0), 'invalid token address'); // 引数のバリデート、0アドレスは無効
+        require(tokenToExchange[_tokenAddress] == address(0), 'exchange already exists'); // すでにExchangeが存在する場合は複数デプロイしない
 
-        Exchange exchange = new Exchange(_tokenAddress);
-        tokenToExchange[_tokenAddress] = address(exchange);
+        Exchange exchange = new Exchange(_tokenAddress); // Exchangeコントラクトをデプロイする、createを使っている
+        tokenToExchange[_tokenAddress] = address(exchange); // マッピングにExchangeのアドレスを記録
 
-        return address(exchange);
+        return address(exchange); // デプロイしたExchangeのアドレスを返す
     }
 
     function getExchange(address _tokenAddress) public view returns (address) {
-        return tokenToExchange[_tokenAddress];
+        return tokenToExchange[_tokenAddress]; // マッピングからExchangeのアドレスを取得
     }
 }
